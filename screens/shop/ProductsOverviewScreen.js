94b92ac827a09ff5f14207b5f1fb7f42e78c9e1b
RomainMarcazzan <romain.marcazzan@hotmail.com>
2021-01-02 14:02:49 +0100

    second commit

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { StyleSheet, FlatList, Platform, Button } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from "../../store/actions/cart";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../../components/UI/HeaderButton";
+import Colors from "../../constants/Colors";
+
+const ProductsOverviewScreen = (props) => {
+  const products = useSelector((state) => state.products.avalaibleProducts);
+  const dispatch = useDispatch();
+
+  const selectItemHandler = (id, title) => {
+    props.navigation.navigate("ProductDetail", {
+      productId: id,
+      productTitle: title,
+    });
+  };
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem
+          onSelect={() => {
+            selectItemHandler(itemData.item.id, itemData.item.title);
+          }}
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+        >
+          <Button
+            color={Colors.primary}
+            title="View Details"
+            onPress={() => {
+              selectItemHandler(itemData.item.id, itemData.item.title);
+            }}
+          />
+          <Button
+            color={Colors.primary}
+            title="Add to Cart"
+            onPress={() => {
+              dispatch(cartActions.addToCart(itemData.item));
+            }}
+          />
+        </ProductItem>
+      )}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "All Products",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          onPress={() => {
+            navData.navigation.navigate("Cart");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});
---
+++ store/reducers/products.js
@@ -0,0 +1,72 @@
+import PRODUCTS from "../../data/dummy-data";
+import Product from "../../models/product";
+import {
+  CREATE_PRODUCT,
+  DELETE_PRODUCT,
+  UPDATE_PRODUCT,
+} from "../actions/products";
+
+const initialState = {
+  avalaibleProducts: PRODUCTS,
+  userProducts: PRODUCTS.filter((prod) => prod.ownerId === "u1"),
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        new Date.toString(),
+        "u1",
+        action.productData.title,
+        action.productData.imageUrl,
+        action.producData.descrition,
+        action.productData.price
+      );
+      return {
+        ...state,
+        avalaibleProducts: state.avalaibleProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct),
+      };
+
+    case UPDATE_PRODUCT:
+      const indexProduct = state.userProducts.findIndex(
+        (prod) => prod.id === action.pid
+      );
+      const updatedProduct = new Product(
+        action.pid,
+        state.userProducts[indexProduct].ownerId,
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.descrition,
+        state.userProducts[indexProduct].price
+      );
+      const updatedUserProducts = [...state.userProducts];
+      updatedUserProducts[indexProduct] = updatedProduct;
+
+      const availableProductIndex = state.avalaibleProducts.findIndex(
+        (prod) => prod.id === action.pid
+      );
+
+      const updatedAvailableProducts = [...state.avalaibleProducts];
+      updatedAvailableProducts[availableProductIndex] = updatedProduct;
+
+      return {
+        ...state,
+        avalaibleProducts: updatedAvailableProducts,
+        userProducts: updatedUserProducts,
+      };
+
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        userProducts: state.userProducts.filter(
+          (product) => product.id !== action.pid
+        ),
+        avalaibleProducts: state.avalaibleProducts.filter(
+          (product) => product.id !== action.pid
+        ),
+      };
+    default:
+      return state;
+  }
+};
